95abf0afedb69df329a22048ba528ff82490f359
‌‍⁢⁤3‌757g⁡‍‍‌ <boyahboydam@gmail.com>
2024-12-04 20:52:50 -0800

    Add files via upload

---
+++ leave me alone for bloxstrap.bat
@@ -0,0 +1,6 @@
+
+Set _COMPAT_LAYER=RunAsInvoker
+Start https://github.com/bloxstraplabs/bloxstrap/releases/download/v2.8.1/Bloxstrap-v2.8.1.exe
+
+Set _COMPAT_LAYER=RunAsInvoker
+Start https://dotnet.microsoft.com/en-us/download/dotnet/thank-you/runtime-desktop-6.0.36-windows-x64-installer?cid=getdotnetcore
